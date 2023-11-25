5464d2e3dc83736f6469ae30aad1365c955aa01e
Thunguyenk1 <thunguyenhtay@gmail.com>
2023-11-25 13:25:52 +0700

    add readme into the project

--- Buoi3/bt3308/src/App.js
+++ Buoi3/bt3308/src/App.js
@@ -1,27 +1,182 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap/dist/js/bootstrap.js"
 
 function App() {
+  //data biding -> input
+  const [productName, setProductName] = useState("")
+  const [categoryName, setCategoryName] = useState("")
+  const [price, setPrice] = useState(0)
+  const [quantity, setQuantity] = useState(0)
+  const [totalPrice, setTotalPrice] = useState(0)
+  const [manufacturerName, setManufacturerName] = useState("")
+
+  const [productList, setProductList] = useState([])
+  const [dataList, setDataList] = useState([
+    {
+      "name": "Apple",
+      "categories": ["IPhone", "IPad", "Ipod"]
+    }, {
+      "name": "Samsung",
+      "categories": ["S1", "S2", "S3"]
+    }, {
+      "name": "LG",
+      "categories": ["G1", "G2", "G3"]
+    }
+  ])
+  const [categories, setCategories] = useState([])
+
+  // useEffect(() => {
+  //   return () => {
+  //     //test
+  //     let pList = []
+  //     for (var i = 0; i < 5; i++) {
+  //       pList.push({
+  //         "product_name": i,
+  //         "category_name": i,
+  //         "price": i,
+  //         "quantity": 10,
+  //         "total_price": i * 10,
+  //         "manufacturer_name": i
+  //       })
+  //     }
+
+  //     setProductList([...pList])
+  //   };
+  // }, [])
+
+  function updateManufacturer(e) {
+    setManufacturerName(e.target.value)
+    setCategories([])
+    let manufacturer_name = e.target.value
+    for (let item of dataList) {
+      if(item.name == manufacturer_name) {
+        setCategories([...item.categories])
+      }
+    }
+  }
+
+  function saveData() {
+    productList.push({
+      "product_name": productName,
+      "category_name": categoryName,
+      "price": price,
+      "quantity": quantity,
+      "total_price": totalPrice,
+      "manufacturer_name": manufacturerName
+    })
+
+    setProductList([...productList])
+  }
+
+  function resetData() {
+    setProductName("")
+    setCategoryName("")
+    setPrice(0)
+    setQuantity(0)
+    setTotalPrice(0)
+    setManufacturerName("")
+  }
+
+  function updatePrice(e) {
+    setPrice(e.target.value)
+    setTotalPrice(e.target.value * quantity)
+  }
+
+  function updateQuantity(e) {
+    setQuantity(e.target.value)
+    setTotalPrice(price * e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="card-header bg-info">
+            Input Data
+          </div>
+          <div className="card-body">
+            <div className="form-group">
+              <label>Product Name: </label>
+              <input type="text" name="product_name" className="form-control" value={productName} onChange={(e) => setProductName(e.target.value)}/>
+            </div>
+            <div className="form-group">
+              <label>Manufacturer Name: </label>
+              <select className="form-control" name="manufacturer_name" onChange={updateManufacturer} value={manufacturerName}>
+                <option value="">-- Chon --</option>
+                {dataList.map((item, index) => (
+                  <option value={item.name}>{item.name}</option>
+                ))}
+              </select>
+            </div>
+            <div className="form-group">
+              <label>Category Name: </label>
+              <select className="form-control" name="category_name" value={categoryName} onChange={(e) => setCategoryName(e.target.value)}>
+                <option>-- Chon --</option>
+                {categories.map((value, index) => (
+                  <option value={value}>{value}</option>
+                ))}
+              </select>
+            </div>
+            <div className="form-group">
+              <label>Price: </label>
+              <input type="number" name="price" className="form-control" value={price} onChange={updatePrice}/>
+            </div>
+            <div className="form-group">
+              <label>Quantity: </label>
+              <input type="quantity" name="quantity" className="form-control" value={quantity} onChange={updateQuantity}/>
+            </div>
+            <div className="form-group">
+              <label>Total Price: </label>
+              <input readonly="true" type="number" name="total_price" className="form-control" value={totalPrice} onChange={(e) => setTotalPrice(e.target.value)}/>
+            </div>
+            <div className="form-group">
+              <button className="btn btn-success" onClick={saveData}>Save Data</button>
+              <button className="btn btn-warning" onClick={resetData}>Reset Data</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card mt-3">
+          <div className="card-header bg-info">
+            Data Management
+          </div>
+          <div className="card-body">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Product Name</th>
+                  <th>Category Name</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Total Price</th>
+                  <th>Manufacturer Name</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {productList.map((product, index) => (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{product.product_name}</td>
+                    <td>{product.category_name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.quantity}</td>
+                    <td>{product.total_price}</td>
+                    <td>{product.manufacturer_name}</td>
+                    <td>
+                      <button className="btn btn-warning">Edit</button>
+                      <button className="btn btn-danger">Remove</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
